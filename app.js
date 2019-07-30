a3c213fd96f01d2fcc67e4ef3b56916dcf3d8948
Edwin Dominguez <edwindominguez@hotmail.com>
2019-07-29 21:49:46 -0500

    comment retrieve function

--- app.js
+++ app.js
@@ -3,35 +3,15 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    // seedDB = require("./seeds"),
     app = express(),
     port = 3000;
 
+// seedDB();
 mongoose.connect("mongodb://localhost:27017/yelpcamp", {useNewUrlParser: true});
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-
-
-//Create a new campground and save to DB
-// Campground.create({
-    // name: "Salmon Creek",
-    // image: "https://i1.wp.com/visitmckenzieriver.com/oregon/wp-content/uploads/2015/06/paradise_campground.jpg",
-    // description: "This is Salmon Creek, very nice play."
-// }, function(error, camp){
-    // if(error){
-        // console.log("error: " + error);
-    // }else{
-        // console.log("New camp added: " + camp);
-    // }
-// });
-
-// var campgrounds = [
-//     {name: "Salmon Creek", image: "https://i1.wp.com/visitmckenzieriver.com/oregon/wp-content/uploads/2015/06/paradise_campground.jpg?fit=640%2C480"},
-//     {name: "Granite Hill", image: "https://ramblinpinescampground.com/images/Home-6-Boat.jpg"},
-//     {name: "Pampa", image: "https://www.springfieldmn.org/vertical/Sites/%7BEAA6D6AB-E9E9-451A-BF61-D6988056DABE%7D/uploads/campground_play_area_1.JPG"},
-//     {name: "Salmon Creek", image: "https://i1.wp.com/visitmckenzieriver.com/oregon/wp-content/uploads/2015/06/paradise_campground.jpg?fit=640%2C480"},
-//     {name: "Granite Hill", image: "https://ramblinpinescampground.com/images/Home-6-Boat.jpg"},
-//     {name: "Pampa", image: "https://www.springfieldmn.org/vertical/Sites/%7BEAA6D6AB-E9E9-451A-BF61-D6988056DABE%7D/uploads/campground_play_area_1.JPG"}
-// ]
 
 app.get("/", function(req,res){
     res.render("landing");
@@ -76,7 +56,7 @@ app.get("/campgrounds/new", function(req, res){
 //SHOW - Shows info about one dog
 app.get("/campgrounds/:id", function(req, res){
     //find the campground with provided ID
-    Campground.findById(req.params.id, function(error, foundCamp){
+    Campground.findById(req.params.id).populate("comments").exec(function(error, foundCamp){
         if(error){
             console.log(error);
         }else{
