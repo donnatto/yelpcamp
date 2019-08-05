2309a221c13cd96e4261e9b6498d6157449797a2
Edwin Dominguez <edwindominguez@hotmail.com>
2019-08-05 01:35:42 -0500

    add environment dbURL

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const commentRoutes = require('./routes/comments');
 const indexRoutes = require('./routes/index');
 
 // seedDB();
-mongoose.connect('mongodb+srv://donnatto:mariagracia@cluster0-eib0p.mongodb.net/yelpcamp?retryWrites=true&w=majority', {
+mongoose.connect(process.env.DATABASEURL, {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
